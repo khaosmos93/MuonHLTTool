13e5cfabeb220f12292541e8c168f6330fc792a4
Kyoengpil Lee <kplee@cern.ch>
2018-12-14 21:08:56 +0100

    example code

---
+++ Analyzer/Example/PrintObject.cxx
@@ -0,0 +1,117 @@
+#include <Include/MuonHLTTool.h>
+
+class Example
+{
+public:
+  vector<TString> vec_ntuplePath_;
+
+  Example()
+  {
+
+  }
+
+  void AddNtuplePath(TString ntuplePath)
+  {
+    vec_ntuplePath_.push_back( ntuplePath );
+  }
+
+  void Run()
+  {
+    StartTimer();
+
+    TChain *chain = new TChain("ntupler/ntuple");
+    for(const auto& ntuplePath : vec_ntuplePath_ )
+      chain->Add( ntuplePath );
+
+    MuonHLT::NtupleHandle *ntuple = new MuonHLT::NtupleHandle(chain);
+    // -- turn-on branches if you need - e.g. TurnOnBranches_Muon(), TurnOnBranches_GenParticle()
+    ntuple->TurnOnBranches_HLTMuon();
+
+    Int_t nEvent = chain->GetEntries();
+    for(Int_t i_ev=0; i_ev<nEvent; i_ev++)
+    {
+      chain->GetEvent(i_ev);
+      cout << "*** " << i_ev << " th event ***" << endl;
+
+      cout << "[List of fired trigger]" << endl;
+      for(const auto& firedTrigger: *ntuple->vec_firedTrigger )
+      {
+        cout << "  " << firedTrigger << endl;
+      }
+
+      cout << "[List of my-fired trigger]" << endl;
+      for(const auto& firedTrigger: *ntuple->vec_myFiredTrigger )
+      {
+        cout << "  " << firedTrigger << endl;
+      }
+
+      vector<MuonHLT::HLTObject> vec_HLTObject = MuonHLT::GetAllHLTObject(ntuple);
+      cout << "[List of all HLT objects]" << endl;
+      for(const auto& HLTObject : vec_HLTObject )
+      {
+        cout << "  (filterName, pt, eta, phi) = ("
+        << HLTObject.filterName << ", "
+        << HLTObject.pt         << ", "
+        << HLTObject.eta        << ", "
+        << HLTObject.phi        << ")" << endl;
+      }
+
+      vector<MuonHLT::MYHLTObject> vec_MYHLTObject = MuonHLT::GetAllMYHLTObject(ntuple);
+      cout << "[List of all MYHLT objects]" << endl;
+      for(const auto& MYHLTObject : vec_MYHLTObject )
+      {
+        cout << "  (filterName, pt, eta, phi) = ("
+        << MYHLTObject.filterName << ", "
+        << MYHLTObject.pt         << ", "
+        << MYHLTObject.eta        << ", "
+        << MYHLTObject.phi        << ")" << endl;
+      }
+
+      vector<MuonHLT::L1Muon> vec_L1Muon = MuonHLT::GetAllL1Muon(ntuple);
+      cout << "[List of all L1 muons]" << endl;
+      for(const auto& l1mu : vec_L1Muon )
+      {
+        cout << "  (pt, eta, phi, charge, quality) = ("
+        << l1mu.pt      << ", "
+        << l1mu.eta     << ", "
+        << l1mu.phi     << ", "
+        << l1mu.charge  << ", "
+        << l1mu.quality << ")" << endl;
+      }
+
+      cout << endl;
+
+      if( i_ev > 100 ) break;
+    }
+
+    PrintRunTime();
+  }
+
+private:
+  TStopwatch timer_;
+
+  void StartTimer()
+  {
+    timer_.Start();
+  }
+
+  void PrintRunTime()
+  {
+    Double_t cpuTime = timer_.CpuTime();
+    Double_t realTime = timer_.RealTime();
+
+    cout << "************************************************" << endl;
+    cout << "Total real time: " << realTime << " (seconds)" << endl;
+    cout << "Total CPU time:  " << cpuTime << " (seconds)" << endl;
+    cout << "  CPU time / real time = " << cpuTime / realTime << endl;
+    cout << "************************************************" << endl;
+  }
+};
+
+void PrintObject()
+{
+  TString ntuplePath = "/Users/KyeongPil_Lee/ServiceWorks/ExampleROOTFile/MuonHLTNtuple_ZMuMu_M50to120.root";
+  Example* example = new Example();
+  example->AddNtuplePath(ntuplePath);
+  example->Run();  
+}
